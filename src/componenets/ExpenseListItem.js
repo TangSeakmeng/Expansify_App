1a1c6ce5b62e191b4b728608df29e98ef1aaa0b4
Tang <TangSeakmeng01@gmail.com>
2020-07-29 00:04:56 +0700

    Edit List Item

--- src/componenets/ExpenseListItem.js
+++ src/componenets/ExpenseListItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import moment from 'moment';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import numeral from 'numeral';
 
 import { removeExpense } from '../actions/expenses';
 
@@ -10,7 +11,7 @@ const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
         <Link to={`/edit/${id}`}>
             <h3>{ description }</h3>
         </Link>
-        <p>{ amount } - { moment(createdAt).format('MM-DD-YYYY') }</p>
+        <p>{ numeral(amount).format('$0,0.00') } - { moment(createdAt).format('MMMM Do, YYYY') }</p>
         
         {/* <button onClick={() => {
             props.dispatch(removeExpense({ id }))
